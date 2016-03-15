5a4c3f975e26e3ede7df40761820fdfa37558c22
vichen <vivian.vc.chen@gmail.com>
2016-03-14 17:38:30 -0700

    working on chatroom names

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,19 +8,35 @@ App.prototype.clearMessages = function() {
 App.prototype.server = 'https://api.parse.com/1/classes/messages';
 
 App.prototype.addRoom = function(roomName) {
-  $('#roomSelect').append('<div><h3>' + roomName + '</h3></div>');
+  $('#selectRoom').append('<option value="' + roomName + '">' + roomName + '</option>');
 };
 
 App.prototype.addMessage = function(message) {
-  var chatItem = $('div').
-  $('#chats').append('<div class="message">' + message.text + '</div>');
+  var $chatItem = $('<div></div>');
+  $chatItem.addClass('chat');
+  $chatItem.addClass(message.roomname);
+  
+  var $username = $('<span></span>');
+  $username.addClass('username'); $username.text(message.username);
+  $chatItem.append($username);
+
+  //add click function to username... call addFriend
+  $username.click( this.addFriend.bind(this, $username.text()) );
+
+  var $message = $('<div></div>'); $message.text(message.text);
+  $chatItem.append($message);
+  
+  $('#chats').append($chatItem);
 };
 
 App.prototype.init = function() {
-  this.fetch();
+  var selectedRoom = $('#selectRoom').val();
+  this.roomlist = [];
+  this.fetch(selectedRoom);
 };
 
-App.prototype.fetch = function() {
+App.prototype.fetch = function(room) {
+  var room = room || 'lobby';
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: this.server,
@@ -29,9 +45,22 @@ App.prototype.fetch = function() {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Messages retrieved.');
+      //clear messages
       this.clearMessages();
+      //update our list of available chatrooms
       data.results.forEach(function(item) {
-        this.addMessage(item);
+        if (!this.roomlist.indexOf(item.roomname)) {
+          this.roomlist.push(item.roomname);
+          //add to roomlist selection <option> field
+          this.addRoom(item.roomName);
+        }
+      }.bind(this));
+
+      //display each message
+      data.results.forEach(function(item) {
+        if (item.roomname === room) {
+          this.addMessage(item);
+        } 
       }.bind(this));
     }.bind(this),
     error: function (data) {
@@ -58,6 +87,32 @@ App.prototype.send = function(message) {
     }
   });
 };
+
+App.prototype.addFriend = function(name) {
+  console.log(name);
+};
+
+App.prototype.handleSubmit = function(message) {
+    //get the message from .userMessage
+  var $userMessage = $('.userMessage').val();
+
+  var grabUser = function(URLtext) {
+    var name = URLtext.match(/&|\?username=(.*)/)[1];
+    return name;
+  };
+
+  var thisURL = window.location.search;
+  var user = grabUser(thisURL);
+  //var message = $('input.userMessage');
+  App.prototype.send({username: user, text: $userMessage });
+  App.prototype.fetch();
+};
+
+
+
+
+
+
 
 var app = new App;
 app.init();
